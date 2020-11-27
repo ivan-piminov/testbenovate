c845ad1ed04a96e40fa4b9ae9caf6ff900ea7752
ivan-piminov <ivan-piminov@yandex.ru>
2020-11-27 11:51:33 +0300

    add https

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api ={
  loadStuff () {
      // запрос за рандомным списком пользователей (есть имя, фамилия, но нет отдела)
-     return axios.get('http://www.filltext.com/?rows=400&id={number|1000}&firstName={firstName}&lastName={lastName}}')
+     return axios.get('https://www.filltext.com/?rows=400&id={number|1000}&firstName={firstName}&lastName={lastName}}')
          .then(res => res.data)
          // каждому элементу добавляем ключ department со сзачением от 0 до 3, где каждой цифре соответствует название отдела
          .then(data => data.map(item => item = {...item, department:Math.floor(Math.random() * (4))}))
